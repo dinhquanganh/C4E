2ff3a66dd68f408703bde2255dc7aab1d50a1629
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2020-06-22 23:57:09 +0700

    up

--- HomeWork/SS3/B7.js
+++ HomeWork/SS3/B7.js
@@ -1,1 +1,9 @@
-const ar = []
+const ar = [3,4,6,-9,10,-88,2];
+let fD = Number(prompt('Enter a number?'));
+let find = ar.indexOf(fD);
+if (find<0) {
+    alert(`${fD} is NOT found in my array`);
+} else {
+    alert(`${fD} is found in my array at index ${find+1}`);
+}
+
