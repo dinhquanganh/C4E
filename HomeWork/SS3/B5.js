45018da385b8cfbc202071a266c0d80cb99d05d6
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2020-06-23 19:24:15 +0700

    p

--- HomeWork/SS3/B5.js
+++ HomeWork/SS3/B5.js
@@ -1,8 +1,7 @@
 let ip= prompt('Enter the consequence of Number, separated by commas (,)');
-let inputN = [];
-inputN= ip.split(",");
+let inputN= ip.split(",").map(Number);
 let sum=0;
-for (let i =0;i< inputN.length;i++){
-    sum+=Number(inputN[i]);
+for (let i = 0;i<inputN.length;i++){
+    sum+=inputN[i];
 }
 alert(`The sum of them ${sum}`);
