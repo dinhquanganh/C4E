444f401fcd14ab59daf4ee5b50fcffb0595e79e5
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2020-06-23 20:18:13 +0700

    Update B11.js

--- HomeWork/SS3/B11.js
+++ HomeWork/SS3/B11.js
@@ -1,4 +1,7 @@
 let inputNum = prompt("Enter senquence of Number");
-let newNum=inputNum.split(",").map(Number).filter(x => x % 2 == 1);
+let newNum=inputNum.split(",").map(Number).filter(number => number % 2 == 1);
 // newNum = newNum.filter(x => x % 2 == 1);
 alert(`${newNum}`);
+
+//
+
