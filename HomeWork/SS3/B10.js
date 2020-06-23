fa00914a2a215a1092b953f23d005f85dc35c530
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2020-06-23 20:06:38 +0700

    update0nepice

--- HomeWork/SS3/B10.js
+++ HomeWork/SS3/B10.js
@@ -1,3 +1,5 @@
 let inputStr = prompt("Enter senquence names");
-let name=inputStr.split(",").map(x=>`<${x}>`);
+let name=inputStr.split(",").map((name) =>{
+    return `<${name}>`});
+// neu chi co 1 lenh thi chi can .map(name => `<${name}`)
 alert(`${inputStr} => ${name}`);    
--- HomeWork/SS3/B11.js
+++ HomeWork/SS3/B11.js
@@ -1,4 +1,4 @@
 let inputNum = prompt("Enter senquence of Number");
-let newNum=inputNum.split(",").map(Number);
-newNum = newNum.filter(x => x % 2 == 1);
+let newNum=inputNum.split(",").map(Number).filter(x => x % 2 == 1);
+// newNum = newNum.filter(x => x % 2 == 1);
 alert(`${newNum}`);
