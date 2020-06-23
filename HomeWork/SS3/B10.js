9aff0bc5d4434b162f8e22e08843684a6c522c41
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2020-06-23 20:08:10 +0700

    Update B10.js

--- HomeWork/SS3/B10.js
+++ HomeWork/SS3/B10.js
@@ -2,4 +2,6 @@ let inputStr = prompt("Enter senquence names");
 let name=inputStr.split(",").map((name) =>{
     return `<${name}>`});
 // neu chi co 1 lenh thi chi can .map(name => `<${name}`)
-alert(`${inputStr} => ${name}`);    
+alert(`${inputStr} => ${name}`.join(' '));  
+// join() :nối các phần tử trong array
+   
