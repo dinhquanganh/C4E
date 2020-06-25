2c95362b477ea274f1ad15545997bb2ba00f1686
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2020-06-25 14:05:08 +0700

    up

--- HomeWork/BTB4/B6.js
+++ HomeWork/BTB4/B6.js
@@ -1,3 +1,12 @@
 let n= Number(prompt('Input N=?'));
 let inputStr= prompt('Input number (,)').split(',');
-console.log(inputStr);
+let newArr=[];
+for (let i=0; i<n; i++) {
+    if (i<inputStr.length) {
+        newArr[i]=inputStr[i];
+    } else {
+        console.log('n lon hon so phan tu cua mang ban vua nhap');
+        break;
+    }
+}
+console.log(newArr);
