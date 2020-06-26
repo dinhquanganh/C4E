037cc63253a2b1ccbbb00ea3e74fc4d0ae3431e0
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2020-06-26 22:16:30 +0700

    update <3

--- HomeWork/BTB4/B3.js
+++ HomeWork/BTB4/B3.js
@@ -1,5 +1,10 @@
 let arrNumber= [1,2,4,6,[3,5],[10,12]];
-let sum = arrNumber.reduce(function(a, b) {
+let arrNew= arrNumber.join(',').split(',').map(Number);
+let sum = arrNew.reduce(function(a, b) {
     return a + b;
 },0);
 console.log(sum);
+for (i in arrNew) {
+    arrNew[i]+=10;
+}
+console.log(arrNew);
