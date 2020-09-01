924138993025783c814d07a7781f2990b5629508
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2020-09-01 17:19:26 +0700

    wd

--- HomeWork/Test/b1.js
+++ HomeWork/Test/b1.js
@@ -1,1 +1,19 @@
-e
+let inputArray=prompt('inputArray,separated by commas (,)').split(',').map(Number);
+let niceArray=[];
+function check(x, y) {
+    return Math.log(y) / Math.log(x);
+}
+for(i=0; i<inputArray.length; i++) {
+    for(let j=2; j<=Math.sqrt(inputArray[i]).toFixed(0); j++) {
+        if (inputArray[i] % j==0) {
+            let fix= check(j,inputArray[i]).toFixed(1);
+            console.log(Number.isInteger(fix));
+            if(Number.isInteger(check(j,inputArray[i])) == true) {
+                niceArray.push(inputArray[i]);
+                break;
+            }
+        }
+}
+}
+
+console.log(niceArray);
